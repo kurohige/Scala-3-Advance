4e0a995eada8caad4c5e1ac3ef9a0a9c4fdbcfbf
Jose Hidalgo <jhidalgopalacios@yahoo.com>
2024-01-16 21:28:16 +0200

    Scala3Advance - starting part 2

--- src/main/scala/com/rockthejvm/part2afp/CurryingPAFs.scala
+++ src/main/scala/com/rockthejvm/part2afp/CurryingPAFs.scala
@@ -2,7 +2,18 @@ package com.rockthejvm.part2afp
 
 object CurryingPAFs {
 
+  // currying
+  val superAdder: Int => Int => Int =
+    x => y => x + y
 
+  val add3: Int => Int = superAdder(3) // y => 3 + y
+  val eight = add3(5)
+  val eight_v2 = superAdder(3)(5) // curried function
+
+  // Curried methods
+  def curriedAdder(x: Int)(y: Int): Int = x + y
+
+  // methods != functions values
 
   def main(args: Array[String]): Unit = {
 
