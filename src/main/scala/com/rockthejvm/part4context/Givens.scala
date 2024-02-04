bf34da7310454b7cf58ad771f4b840cf8b45b062
Jose Hidalgo <jhidalgopalacios@yahoo.com>
2024-02-04 22:37:37 +0200

    Scala3Advance - starting part 2

--- src/main/scala/com/rockthejvm/part4context/Givens.scala
+++ src/main/scala/com/rockthejvm/part4context/Givens.scala
@@ -51,6 +51,39 @@ object Givens {
   val firstSum = combineAll(List(1, 2, 3))//(intCombinator) <-- passed automatically
   // al combineAllPeople = combineAll(people) // doesn't compile - no Combinator[Person] in scope
 
+  // context bound
+  def combineInGroupsOf3[A](list: List[A])(using Combinator[A]): List[A] = {
+    list.grouped(3).map(group => combineAll(group)/*(combinator) passed by the compiler*/).toList
+  }
+
+  def combineInGroupsOf3V2[A: Combinator](list: List[A]): List[A] = {
+    list.grouped(3).map(group => combineAll(group) /*(combinator) passed by the compiler*/).toList
+  }
+
+  // synthesize new given instance based on existing ones
+  given listOrdering(using intOrdering: Ordering[Int]): Ordering[List[Int]] with {
+    override def compare(x: List[Int], y: List[Int]): Int = {
+      x.sum - y.sum
+    }
+  }
+
+  val listOfList = List(List(1,2,3), List(4,5,6), List(7,8,9))
+  val nestedListOrdered = listOfList.sorted // (listOrdering(intOrdering)) passed by the compiler
+
+  // ... with generics
+  given listOrderingBasedOnCombinator[A](using ord: Ordering[A])(using combinator: Combinator[A]): Ordering[List[A]] with {
+    override def compare(x: List[A], y: List[A]) = {
+      ord.compare(combineAll(x), combineAll(y))
+    }
+  }
+
+  // pass a regular value instead of a given
+  val myCombinator = new Combinator[Int] {
+    override def combine(x: Int, y: Int): Int = x * y
+  }
+  val listProduct = combineAll(List(1,2,3))(using myCombinator)
+
+
   def main(args: Array[String]): Unit = {}
 
 }
