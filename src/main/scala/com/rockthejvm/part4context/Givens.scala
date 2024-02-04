8dbdb398b7d992279a5a04159d0f8dcadf35b7fe
Jose Hidalgo <jhidalgopalacios@yahoo.com>
2024-02-04 22:03:17 +0200

    Scala3Advance - starting part 2

---
+++ src/main/scala/com/rockthejvm/part4context/Givens.scala
@@ -0,0 +1,11 @@
+package com.rockthejvm.part4context
+
+object Givens {
+
+  // list sorting
+  val aList = List(2, 1, 3)
+  val anOrderedList = aList.sorted
+
+  def main(args: Array[String]): Unit = {}
+
+}
