fdb02ffc3d76eb46f759b0ec514e9516f70e2748
Jose Hidalgo <jhidalgopalacios@yahoo.com>
2024-02-11 00:08:16 +0200

    Scala3Advance - starting part 2

--- src/main/scala/com/rockthejvm/part4context/TypeClasses.scala
+++ src/main/scala/com/rockthejvm/part4context/TypeClasses.scala
@@ -15,7 +15,8 @@ object TypeClasses {
     override def toHTML: String = s"<div>$name ($age yo) <a href=$email/></div>"
   }
 
-  val bob2HTML = User("Bob", 43, "bob@rockthejvm.com").toHTML
+  val bob = User("Bob", 43, "bob@rockthejvm.com")
+  val bob2HTML = bob.toHTML
   // same for other data structures that we want to serialize
 
   /*
@@ -40,6 +41,50 @@ object TypeClasses {
     - still only one implementation
    */
 
+  // v3: type class
+  trait HTMLSerializer[T] {
+    def serialize(value: T): String
+  }
+
+  // part 2 - type class instances for the supported types
+  val userSerializer = new HTMLSerializer[User] {
+    override def serialize(value: User): String =
+      s"<div>${value.name} (${value.age} yo) <a href=${value.email}/></div>"
+  }
+
+  val bob2HTML_V2 = userSerializer.serialize(bob)
+
+  /*
+    Benefits:
+    - can define serializers for other types OUTSIDE the "library"
+    - multiple implementations for a type, pick whichever you want
+   */
+
+  import java.util.Date
+  object DateSerializer extends HTMLSerializer[Date] {
+    override def serialize(value: Date): String =
+      s"<div>${value.toString}</div>"
+  }
+
+  val partialUserSerializer = new HTMLSerializer[User] {
+    override def serialize(value: User): String =
+      s"<div>${value.name}</div>"
+  }
+
+  object SomeOtherSerializerFunctionality { // organize givens properly
+    given SomeOtherSerializerFunctionality :HTMLSerializer[User] with {
+      override def serialize(value: User): String =
+        s"<div>${value.name}</div>"
+    }
+  }
+
+  // part 3 - using the type class (user-facing API)
+  object HTMLSerializer {
+    def serialize[T](value: T)(using serializer: HTMLSerializer[T]): String =
+    serializer.serialize(value)
+  }
+
+  val bob2HTML_V3 = HTMLSerializer.serialize(bob)
 
 
   def main(args: Array[String]): Unit = {}
