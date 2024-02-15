4841f4e5d85c677ee3ae09babe5a02d9c11fa35d
Jose Hidalgo <jhidalgopalacios@yahoo.com>
2024-02-15 21:52:56 +0200

    Scala3Advance - starting part 5

---
+++ src/main/scala/com/rockthejvm/part5ts/AdvanceInheritance.scala
@@ -0,0 +1,68 @@
+package com.rockthejvm.part5ts
+
+object AdvanceInheritance {
+
+  // 1 - composite types can be used on their own
+  trait Writer[T] {
+    def write(value: T): Unit
+  }
+
+  trait Stream[T] {
+    def foreach(f: T => Unit): Unit
+  }
+
+  trait Closeable {
+    def close(status: Int): Unit
+  }
+
+  def processStream[T](
+      stream: Stream[T] with Closeable with Writer[T]
+  ): Unit = {
+    stream.foreach(stream.write)
+    stream.close(0)
+  }
+
+  // 2 - diamond problem
+  trait Animal {
+    def name: String
+  }
+
+  trait Lion extends Animal {
+    override def name: String = "lion"
+  }
+  trait Tiger extends Animal {
+    override def name: String = "tiger"
+  }
+
+  class Liger extends Lion with Tiger
+
+  def demoLiger(): Unit = {
+    val liger = new Liger
+    println(liger.name) // tiger
+  }
+
+  // 3 - the super problems
+
+  trait Cold {
+    def print: Unit = println("cold")
+  }
+
+  trait Green extends Cold {
+    override def print: Unit = {
+      println("green")
+      super.print
+    }
+  }
+
+  trait Blue extends Cold {
+    override def print: Unit = {
+      println("blue")
+      super.print
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    demoLiger()
+  }
+
+}
