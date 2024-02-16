0dbebe969ddc04b19378b5102a18f8ae4a3ffbde
Jose Hidalgo <jhidalgopalacios@yahoo.com>
2024-02-16 07:21:20 +0200

    Scala3Advance - starting part 5

--- src/main/scala/com/rockthejvm/part5ts/Variance.scala
+++ src/main/scala/com/rockthejvm/part5ts/Variance.scala
@@ -20,6 +20,56 @@ object Variance {
     laika
   ) // List is COVARIANT: a list of dogs is a list of animals
 
+  class MyList[+A] // adding a "+" makes MyList covariant
+
+  val aListOfAnimals: MyList[Animal] =
+    new MyList[Dog] // ok, MyList is covariant
+
+  // if NO, then the type is INVARIANT
+  trait semiGroup[A] { // no marker = INVARIANT
+    def combine(x: A, y: A): A
+  }
+
+  // java generics are INVARIANT
+  //val aJavaList: java.util.ArrayList[Animal] = new java.util.ArrayList[Dog] // not ok, type mismatch: java generics are all INVARIANT
+
+  // Hell NO - CONTRAVARIANT
+  trait Vet[-A] {
+    def heal(x: A): Boolean
+  }
+
+  val myVet: Vet[Dog] = new Vet[Animal] {
+    override def heal(x: Animal): Boolean = {
+      println("I'm healing an animal")
+      true
+    }
+  }
+  /*
+    Rule of thumb:
+    - if your type PRODUCES or RETRIEVES a value( e.g. a list), then it should be COVARIANT
+    - if your type ACTS ON or CONSUMES a value (e.g. a semiGroup), then it should be CONTRAVARIANT
+    - if your type does neither, then it should be INVARIANT
+   */
+
+  /** Exercises
+    */
+  // 1 - which type should be invariant, covariant, contravariant
+  class RandomGenerator[+A] // covariant
+  class MyFunction1[-A, +B] // contravariant in A, covariant in B
+  class MyOption[+A] // covariant
+  class JSONSerializer[-A] // contravariant
+
+  // 2 - add variance modifiers to this "Library"
+  abstract class LList[A] {
+    def head: A
+    def tail: LList[A]
+  }
+
+  case class EmptyList[A]() extends LList[A] {
+    override def head: A = throw new NoSuchElementException
+    override def tail: LList[A] = throw new NoSuchElementException
+  }
+
   def main(args: Array[String]): Unit = {}
 
 }
