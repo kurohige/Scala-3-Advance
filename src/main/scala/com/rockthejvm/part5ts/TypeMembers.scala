c86413d4d817cda90eec095867cc3d02499cb0f3
Jose Hidalgo <jhidalgopalacios@yahoo.com>
2024-02-21 21:50:20 +0200

    Scala3Advance - starting part 5

--- src/main/scala/com/rockthejvm/part5ts/TypeMembers.scala
+++ src/main/scala/com/rockthejvm/part5ts/TypeMembers.scala
@@ -8,10 +8,21 @@ object TypeMembers {
 
   class AnimalCollection {
     type AnimalType // abstract type member
-    type BoundedAnimal <: Animal // upper bounded type member
+    type BoundedAnimal <: Animal // upper bounded type member (Animal or a subtype)
     type SuperBoundedAnimal >: Dog <: Animal // lower and upper bounded type member
+    type AnimalAlias = Cat // type alias
     type AnimalCage[T <: Animal] // type member with a type parameter
+    type NestedOption =
+      List[Option[Option[Int]]] // often used to alias complex/nested types
   }
+
+  // using type members
+  val ac = new AnimalCollection
+  val anAnimal: ac.AnimalType = ???
+
+  //val cat: ac.BoundedAnimal = new Cat // code does not compile because BoundedAnimal might be Dog
+  val cat: ac.AnimalAlias = new Cat // compiles
+  val aDog: ac.SuperBoundedAnimal = new Dog // compiles
 
   def main(args: Array[String]): Unit = {}
 
