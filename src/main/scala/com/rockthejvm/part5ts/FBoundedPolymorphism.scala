d24edde57d38ead6e65f3821f9c63c3fb7c853c9
Jose Hidalgo <jhidalgopalacios@yahoo.com>
2024-02-25 14:42:34 +0200

    Scala3Advance - starting part 5

--- src/main/scala/com/rockthejvm/part5ts/FBoundedPolymorphism.scala
+++ src/main/scala/com/rockthejvm/part5ts/FBoundedPolymorphism.scala
@@ -2,5 +2,54 @@ package com.rockthejvm.part5ts
 
 object FBoundedPolymorphism {
 
+  object Problem {
+    trait Animal {
+      def breed: List[Animal]
+    }
+
+    class Cat extends Animal {
+      override def breed: List[Animal] = List(new Cat, new Dog) // <- problem!!
+    }
+
+    class Dog extends Animal {
+      override def breed: List[Animal] =
+        List(new Dog, new Dog, new Dog) // List[Dog]
+    }
+    /// losing type safety
+  }
+
+  object NaiveSolution {
+    trait Animal {
+      def breed: List[Animal]
+    }
+
+    class Cat extends Animal {
+      override def breed: List[Cat] = List(new Cat, new Cat)
+    }
+
+    class Dog extends Animal {
+      override def breed: List[Animal] =
+        List(new Dog, new Dog, new Dog) // List[Dog]
+    }
+    // I have to write the proper type signatures
+    // problem: want the compiler to help
+  }
+  
+    object FBP {
+        trait Animal[A <: Animal[A]] { // recursive type F-Bounded Polymorphism
+        def breed: List[A]
+        }
+    
+        class Cat extends Animal[Cat] {
+        override def breed: List[Cat] = List(new Cat, new Cat)
+        }
+    
+        class Dog extends Animal[Dog] {
+        override def breed: List[Dog] =
+            List(new Dog, new Dog, new Dog) // List[Dog]
+        }
+        // type safety
+    }
+
   def main(args: Array[String]): Unit = {}
 }
